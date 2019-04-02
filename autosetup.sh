754a227a8bbc57aab2e240a01caaaf66c275d217
nieto@talos <nieto@gae.ucm.es>
2019-04-02 20:02:32 +0200

    Excluding dl1-data-handler from the macos installation

--- autosetup.sh
+++ autosetup.sh
@@ -6,15 +6,16 @@ fi
 mydir=`pwd`
 dl1dh_ver="master"
 ctlearn_ver="tf-cv-upgrade"
-data_file="data.tgz"
 data_web="http://sagan.gae.ucm.es/~nieto/tmp2/ctlearn-aswg19/"
 
 #Create and activate conda environment
 
 if [[ $1 == "linux" ]]; then
     conda env create -f environment-linux.yml
+    data_file="data-linux.tgz"
 elif [[ $1 == "macos" ]]; then
     conda env create -f environment-macos.yml
+    data_file="data-macos.tgz"
 else
     echo "Supported OS: linux, macos"
     exit
@@ -24,15 +25,18 @@ source activate ctlearn-aswg19
 mkdir -p $mydir"/code"
 cd $mydir"/code"
 
-#Install dl1-data-handler
-if [ ! -d $mydir"/code/dl1-data-handler" ]; then
-    git clone https://github.com/cta-observatory/dl1-data-handler.git
+#Install dl1-data-handler (only under linux)
+
+if [[ $1 == "linux" ]]; then
+    if [ ! -d $mydir"/code/dl1-data-handler" ]; then
+	git clone https://github.com/cta-observatory/dl1-data-handler.git
+    fi
+    cd $mydir"/code/dl1-data-handler"
+    git pull
+    git checkout $dl1dh_ver
+    pip install --upgrade .
+    cd $mydir"/code"
 fi
-cd $mydir"/code/dl1-data-handler"
-git pull
-git checkout $dl1dh_ver
-pip install --upgrade .
-cd $mydir/code
 
 #Install ctlearn
 if [ ! -d $mydir"/code/ctlearn" ]; then
@@ -47,9 +51,10 @@ pip install --upgrade .
 mkdir -p $mydir/data
 cd $mydir/data/
 if [ ! -e $mydir/data/$data_file ]; then 
-    wget http://sagan.gae.ucm.es/~nieto/tmp2/ctlearn-aswg19/data.tgz
+    wget $data_web/$data_file
 fi
 tar -xvzf $mydir/data/$data_file
+cd $mydir
 
 #Check versions
 echo " "
